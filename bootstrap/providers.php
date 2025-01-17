524a766a0e884748360cb461ed0c681d31d77640
Mark Gregory <162308433+mark-mpba@users.noreply.github.com>
2025-01-17 11:16:29 +0000

    new way of registering providers

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,6 +3,5 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\FortifyServiceProvider::class,
-    App\Providers\JetstreamServiceProvider::class,
-    \FloatingPoint\Stylist\StylistServiceProvider::class
+    App\Providers\JetstreamServiceProvider::class
 ];
